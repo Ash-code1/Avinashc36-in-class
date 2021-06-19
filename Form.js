464676834ec1a4e7ae731c31460f188624ff710a
Ash-code1 <77171720+Ash-code1@users.noreply.github.com>
2021-06-19 16:06:00 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,34 @@
+class Form {
+    constructor() {
+      
+    }
+  
+    display(){
+      var title = createElement('h2')
+      title.html("Car Racing Game");
+      title.position(450, 100);
+      
+      var input = createInput("Name");
+      var button = createButton('Play');
+      
+      input.position(430, 260);
+      button.position(500, 300);
+  
+      button.mousePressed(function(){
+        input.hide();
+        button.hide();
+  
+        var name = input.value();
+        
+        playerCount+=1;
+        player.update(name)
+        player.updateCount(playerCount);
+
+    
+        var greeting = createElement('h3');
+        greeting.html("Hello " + name )
+        greeting.position(480, 260)
+      });
+  
+    }
+  }
